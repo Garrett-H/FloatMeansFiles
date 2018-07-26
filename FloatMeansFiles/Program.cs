09ecebf8dc897ae9aadc8e99509cbc322be522d4
Garrett Hay <garlhay@ut.utm.edu>
2018-07-25 22:15:48 -0500

    First add of Program & stacker.cs(s)

---
+++ FloatMeansFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatMeansFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName;
+            int fileNum;
+
+            //grab the file name group
+            Console.WriteLine("Input the name of the group of pictures to open:");
+            fileName = Console.ReadLine();
+            //grab the number of files
+            Console.WriteLine("input the number of files in group: ");
+            fileNum = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        }
+    }
+}
---
+++ FloatMeansFiles/stacker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatMeansFiles
+{
+    class stacker
+    {
+        //private variables
+        private string magic_number;
+        private int width;
+        private int height;
+        private int max_color;
+        private int numOfPictures;
+        private string groupFormat;
+        private struct Pixel
+        {
+            private int red;
+            private int green;
+            private int blue;
+        };
+
+        //public functions
+        public stacker(int numOfFiles, string groupName)
+        {
+            numOfPictures = numOfFiles;
+            groupFormat = groupName;
+
+        }
+
+        public void FileMan(int numOfPics, string fileName)
+        {
+
+        }
+
+        public void FileReader(string inFile_Name, int whichPicture_inputting)
+        {
+
+        }
+
+
+
+    }
+}
