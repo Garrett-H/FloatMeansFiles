a872032c4c4bdeda8b01740fa8f64c9c7edd744b
Garrett Hay <garlhay@ut.utm.edu>
2018-07-25 23:21:56 -0500

    Started on Constructor & File Manager

--- FloatMeansFiles/stacker.cs
+++ FloatMeansFiles/stacker.cs
@@ -15,12 +15,15 @@ namespace FloatMeansFiles
         private int max_color;
         private int numOfPictures;
         private string groupFormat;
-        private struct Pixel
+        private class Pixel
         {
             private int red;
             private int green;
             private int blue;
         };
+
+        List<List<List<Pixel>>> folderOfGrids;
+        List<List<Pixel>> pixelFinal;
 
         //public functions
         public stacker(int numOfFiles, string groupName)
@@ -30,9 +33,40 @@ namespace FloatMeansFiles
 
         }
 
-        public void FileMan(int numOfPics, string fileName)
+        public void FileMan()
         {
+            //grouping all file names under readInFIleName List
+            List<string> readInFileName = new List<string>();
+            for (int i = 0; i < numOfPictures; i++)
+            {
+                if (i<11)
+                {
+                    readInFileName.Add($"{groupFormat}_00{i++}.ppm");
+                }
+                else if (i<101)
+                {
+                    readInFileName.Add($"{groupFormat}_0{i++}.ppm");
+                }
+                else
+                {
+                    readInFileName.Add($"{groupFormat}_{i++}.ppm");
+                }
+            }
+            readInFileName.TrimExcess();
 
+            Console.WriteLine("Stacking images:");
+            //file Reader used
+            int count = 0;
+            foreach (string element in readInFileName)
+            {
+                Console.WriteLine($"{groupFormat}/{element}");
+                FileReader(element, count);
+                count++;
+            }
+
+            //reducing list count
+            readInFileName.Clear();
+            readInFileName.TrimExcess();
         }
 
         public void FileReader(string inFile_Name, int whichPicture_inputting)
@@ -40,7 +74,15 @@ namespace FloatMeansFiles
 
         }
 
+        public void FileSender(string fileName)
+        {
 
+        }
+
+        public void AverageCal()
+        {
+
+        }
 
     }
 }
